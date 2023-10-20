f18725c933e02296ed5dc62d62d003e156ad918d
h.kheirollahpour <h.kheirollahpour@bluteam.ir>
2023-10-20 23:34:25 +0330

    Complete symmetric fragment/layout/viewmodel

--- app/src/main/java/com/hkh/androiddatasecurity/ui/symmetric/SymmetricFragment.kt
+++ app/src/main/java/com/hkh/androiddatasecurity/ui/symmetric/SymmetricFragment.kt
@@ -1,39 +1,164 @@
 package com.hkh.androiddatasecurity.ui.symmetric
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.biometric.BiometricManager
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.hkh.androiddatasecurity.R
+import com.hkh.androiddatasecurity.common.Constant.AUTHENTICATORS
+import com.hkh.androiddatasecurity.common.Constant.KEY_ALIAS_SYMMETRIC
+import com.hkh.androiddatasecurity.common.FingerprintPrompt
 import com.hkh.androiddatasecurity.databinding.FragmentSymmetricBinding
 
-class SymmetricFragment: Fragment() {
+
+class SymmetricFragment : Fragment() {
+
+    private val canAuthenticate by lazy {
+        BiometricManager.from(requireContext().applicationContext)
+            .canAuthenticate(AUTHENTICATORS) == BiometricManager.BIOMETRIC_SUCCESS
+    }
 
     private var _binding: FragmentSymmetricBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: SymmetricViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val symmetricViewModel =
-            ViewModelProvider(this)[SymmetricViewModel::class.java]
-
+        viewModel = ViewModelProvider(this)[SymmetricViewModel::class.java]
         _binding = FragmentSymmetricBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        initActions()
+        return binding.root
+    }
 
-        symmetricViewModel.text.observe(viewLifecycleOwner) {
-            binding.symmetricText.text = it
+    private fun initActions() {
+        observeIsKeyExist()
+        observeShowErrorMessage()
+        observeCheckKeyGeneration()
+        observeCheckKeyRemove()
+        observeCheckEncryptMessage()
+        observeCheckDecryptMessage()
+
+        checkKeyStatus()
+        setupViews()
+    }
+
+    private fun observeIsKeyExist() = viewModel.isKeyExist.observe(viewLifecycleOwner) {
+        showToast(getString(if (it) R.string.key_is_exist else R.string.key_was_not_exist))
+    }
+
+    private fun observeShowErrorMessage() = viewModel.showErrorMessage.observe(viewLifecycleOwner) {
+        showToast(getString(it))
+    }
+
+    private fun observeCheckKeyGeneration() =
+        viewModel.checkKeyGeneration.observe(viewLifecycleOwner) {
+            if (it) resetAllData(true)
+            checkKeyStatus()
         }
 
-        return root
+    private fun observeCheckKeyRemove() =
+        viewModel.checkKeyRemove.observe(viewLifecycleOwner) {
+            if (it) resetAllData(true)
+            checkKeyStatus()
+        }
+
+    private fun observeCheckEncryptMessage() =
+        viewModel.checkEncryptMessage.observe(viewLifecycleOwner) { userInputText ->
+            openBiometric {
+                viewModel.encryptData(userInputText)
+                binding.encryptedTextView.text = viewModel.sealedData?.getBase64CipherData()?.trim()
+                resetDecryptedText()
+            }
+        }
+
+    private fun observeCheckDecryptMessage() =
+        viewModel.checkDecryptMessage.observe(viewLifecycleOwner) { encryptedText ->
+            openBiometric {
+                viewModel.decryptData(encryptedText)
+                binding.decryptedTextView.text = viewModel.decryptedData
+                viewModel.sealedData = null
+            }
+        }
+
+    private fun setupViews() = with(binding) {
+        if (!canAuthenticate) errorView.visibility = View.VISIBLE
+        generateKeyButton.setOnClickListener {
+            viewModel.checkKeyGeneration()
+        }
+        removeKeyButton.setOnClickListener {
+            viewModel.checkKeyRemove()
+        }
+        encryptKeyButton.setOnClickListener {
+            viewModel.checkEncryption(binding.userInputEditText.text.toString())
+        }
+        decryptKeyButton.setOnClickListener {
+            viewModel.checkDecryption(
+                binding.encryptedTextView.text.toString(),
+                getString(R.string.unknown_encrypted)
+            )
+        }
+        userInputEditText.addTextChangedListener {
+            resetAllData(false)
+        }
+    }
+
+    private fun resetAllData(canResetInput: Boolean) {
+        resetEncryptedText()
+        resetDecryptedText()
+        viewModel.sealedData = null
+        viewModel.decryptedData = null
+        if (canResetInput) binding.userInputEditText.setText("")
+    }
+
+    private fun resetEncryptedText() =
+        binding.encryptedTextView.setText(getString(R.string.unknown_encrypted))
+
+    private fun resetDecryptedText() =
+        binding.decryptedTextView.setText(getString(R.string.unknown_decrypted))
+
+    private fun checkKeyStatus() = binding.keyStatusTextView.apply {
+        text = if (viewModel.keyStoreManager.isKeyExist(KEY_ALIAS_SYMMETRIC)) {
+            setTextColor(GREEN_COLOR)
+            getString(R.string.key_is_exist)
+        } else {
+            setTextColor(RED_COLOR)
+            getString(R.string.key_was_not_exist)
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun openBiometric(onSuccess: () -> Unit) {
+        FingerprintPrompt.show(requireActivity()).observe(viewLifecycleOwner) { result ->
+            if (result.isSuccess) {
+                onSuccess.invoke()
+            } else {
+                if (result.isFailedToReadFingerPrint())
+                    showToast(getString(R.string.failed_to_read_biometric))
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        val GREEN_COLOR = Color.argb(255, 33, 103, 94)
+        val RED_COLOR = Color.argb(255, 188, 19, 31)
     }
 
 }
--- app/src/main/java/com/hkh/androiddatasecurity/ui/symmetric/SymmetricViewModel.kt
+++ app/src/main/java/com/hkh/androiddatasecurity/ui/symmetric/SymmetricViewModel.kt
@@ -3,11 +3,93 @@ package com.hkh.androiddatasecurity.ui.symmetric
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.hkh.androiddatasecurity.R
+import com.hkh.androiddatasecurity.common.Constant
+import com.hkh.androiddatasecurity.common.KeyStoreManager
+import com.hkh.androiddatasecurity.common.SymmetricKeyGenerationUtil
+import com.hkh.androiddatasecurity.data.SealedData
 
 class SymmetricViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "We are using reversible-symmetric way with AES-256 algorithm data encryption/decryption"
+    val keyStoreManager by lazy {
+        KeyStoreManager()
     }
-    val text: LiveData<String> = _text
+    private val symmetricKeyGenerationUtil by lazy {
+        SymmetricKeyGenerationUtil(keyStoreManager)
+    }
+
+    var sealedData: SealedData? = null
+    var decryptedData: String? = null
+
+    private val _isKeyExist = MutableLiveData<Boolean>()
+    val isKeyExist: LiveData<Boolean> = _isKeyExist
+
+    private val _showErrorMessage = MutableLiveData<Int>()
+    val showErrorMessage: LiveData<Int> = _showErrorMessage
+
+
+    private val _checkKeyGeneration = MutableLiveData<Boolean>()
+    val checkKeyGeneration: LiveData<Boolean> = _checkKeyGeneration
+
+    fun checkKeyGeneration() {
+        if (!keyStoreManager.isKeyExist(Constant.KEY_ALIAS_SYMMETRIC)) {
+            symmetricKeyGenerationUtil.getOrGenerateKey()
+            _checkKeyGeneration.value = true
+        } else {
+            _isKeyExist.value = true
+        }
+    }
+
+    private val _checkKeyRemove = MutableLiveData<Boolean>()
+    val checkKeyRemove: LiveData<Boolean> = _checkKeyRemove
+    fun checkKeyRemove() {
+        if (keyStoreManager.isKeyExist(Constant.KEY_ALIAS_SYMMETRIC)) {
+            keyStoreManager.removeKey(Constant.KEY_ALIAS_SYMMETRIC)
+            _checkKeyRemove.value = true
+        } else {
+            _isKeyExist.value = false
+        }
+    }
+
+
+    private val _checkEncryptMessage = MutableLiveData<String>()
+    val checkEncryptMessage: LiveData<String> = _checkEncryptMessage
+    fun checkEncryption(userInputText: String) {
+        if (userInputText.isNotEmpty()) {
+            if (keyStoreManager.isKeyExist(Constant.KEY_ALIAS_SYMMETRIC)) {
+                _checkEncryptMessage.value = userInputText
+            } else {
+                _isKeyExist.value = false
+            }
+        } else {
+            _showErrorMessage.value = R.string.user_input_is_empty
+        }
+    }
+
+    fun encryptData(userInputText: String) {
+        sealedData = symmetricKeyGenerationUtil.encrypt(userInputText)
+    }
+
+
+    private val _checkDecryptMessage = MutableLiveData<String>()
+    val checkDecryptMessage: LiveData<String> = _checkDecryptMessage
+    fun checkDecryption(encryptedText: String, defaultText: String) {
+        if (encryptedText != defaultText) {
+            if (keyStoreManager.isKeyExist(Constant.KEY_ALIAS_SYMMETRIC)) {
+                _checkDecryptMessage.value = encryptedText
+            } else {
+                _isKeyExist.value = false
+            }
+        } else {
+            _showErrorMessage.value = R.string.encrypted_text_is_empty
+        }
+    }
+
+    fun decryptData(encryptedText: String) {
+        decryptedData = symmetricKeyGenerationUtil.decrypt(
+            encryptedBase64EncodedText = encryptedText,
+            iv = sealedData?.initialVector
+        )
+    }
+
 }
