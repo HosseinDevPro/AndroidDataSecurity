dbc54beb0c9ade1dfc1f336582da4ba2863c5c66
h.kheirollahpour <h.kheirollahpour@bluteam.ir>
2023-10-20 17:47:10 +0330

    Add sealed data class

---
+++ app/src/main/java/com/hkh/androiddatasecurity/data/SealedData.kt
@@ -0,0 +1,30 @@
+package com.hkh.androiddatasecurity.data
+
+import com.hkh.androiddatasecurity.common.Utils.base64Encode
+
+data class SealedData(
+    val cipheredBytes: ByteArray,
+    val initialVector: ByteArray
+) {
+
+    fun getBase64CipherData() = cipheredBytes.base64Encode()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SealedData
+
+        if (!cipheredBytes.contentEquals(other.cipheredBytes)) return false
+        if (!initialVector.contentEquals(other.initialVector)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = cipheredBytes.contentHashCode()
+        result = 31 * result + initialVector.contentHashCode()
+        return result
+    }
+
+}
